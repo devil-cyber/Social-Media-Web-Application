4d3158ab5ff8452a5835804e220307f91da0ce9a
devil-cyber <mani2474695@gmail.com>
2020-04-23 14:34:35 +0530

    install express layout and setup partial

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(req, res) {
-    return res.end("<h1>Profile</h1>");
+    return res.render("user_profile", {
+        title: "Profile"
+    });
 }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+app.use(express.static("./assets"));
+app.use(expressLayouts);
 app.use("/", require("./routes"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
