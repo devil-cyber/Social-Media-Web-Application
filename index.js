884950f204828d447169d8c46a0fa600babd1ee7
devil-cyber <mani2474695@gmail.com>
2020-04-23 12:53:08 +0530

    create the ejs file for homepage and render it

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 app.use("/", require("./routes"));
-
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 
 
