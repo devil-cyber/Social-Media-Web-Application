028c7233423ee0a1976dff2c93f1a6c6e841efbd
devil-cyber <mani2474695@gmail.com>
2020-04-24 11:10:52 +0530

    setup Signup and Sign In page and create the Schema for the same

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,30 @@ module.exports.profile = function(req, res) {
         title: "Profile"
     });
 }
+
+
+// render SignUp page
+module.exports.SignUp = function(req, res) {
+    return res.render("user_sign_up", {
+        title: "Codeial ! SignUp"
+    });
+}
+
+// render SignIn page
+module.exports.SignIn = function(req, res) {
+    return res.render("user_sign_in", {
+        title: "Codeial ! SignIn"
+    });
+}
+
+// get the sign-up data
+
+module.exports.create = function(req, res) {
+
+}
+
+//create session
+
+module.exports.create_session = function(req, res) {
+
+}
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,14 @@ const userController = require("../controllers/users_controller");
 router.get("/profile", userController.profile);
 
 
+// sign in
+
+router.get("/sign-in", userController.SignIn);
+
+// sign-up
+
+router.get("/sign-up", userController.SignUp);
+
 
 
 
